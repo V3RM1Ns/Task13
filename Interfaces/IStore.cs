2ad545e6131ef34ed4224d6e1061986cc21028e2
V3RM1Ns <rovsanoglu@gmail.com>
2025-05-19 18:58:23 +0400

    refactor:Task13

--- Interfaces/IStore.cs
+++ Interfaces/IStore.cs
@@ -3,9 +3,9 @@ namespace Task13.Interfaces
 {
     public interface IStore
     {
-        List<Product> Products { get; }
+        List<Product> Products { get; set; }
         int ProductLimit { get; set; }
-        double TotalIncome { get; }
+        double TotalIncome { get; set; }
 
         void AddProduct(Product product);
         void SellProduct(string name);
